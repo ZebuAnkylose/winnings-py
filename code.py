d436728a21c51d9c6ea2c73fd8ed4d4818e61200
ZebuAnkylose <di_lisi.alexis@icloud.com>
2015-05-24 16:05:08 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,27 @@
 try:
-  list=[]
+  varlist=[]
   counter=0
   reader = csv.reader(file, 'excel-fr')
   for column in reader:
-    list.append([])
-    list[counter].append(column[X])
-    list[counter].append(column[X+1])
-    list[counter].append(column[X+2])
-    list[counter].append(column[X+3])
-    list[counter].append(column[X+4])
-    list[counter].append(column[X+5])
-    list[counter].append(column[X+6])
+    varlist.append([])
+    varlist[counter].append(column[X])
+    varlist[counter].append(column[X+1])
+    varlist[counter].append(column[X+2])
+    varlist[counter].append(column[X+3])
+    varlist[counter].append(column[X+4])
+    varlist[counter].append(column[X+5])
+    varlist[counter].append(column[X+6])
     counter+=1
+finally:
+    file.close()
+    
+    length=len(varlist)
+    for var1 in range(length): #nb sous listes
+      for var2 in range(7): #X
+        varlist[var1][var2]=int(varlist[var1][var2])
+    varlist_v1v0 #tri selon varlist[var1][0]
+    for var3 in range(1,51): #Tri selon 51 premieres valeurs possible
+      varlist_v1v0.append([])
+      for var1 in range(length):
+          if varlist[var1][0]==var3:
+            varlist_v1v0[var3-1].append(varlist[var1])
